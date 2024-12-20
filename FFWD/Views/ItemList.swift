e64daa24ced7025a657e51d940af0ed55ccecb95
JDeWeese <jdeweese574@icloud.com>
2024-12-19 21:48:23 -0600

    morer

---
+++ FFWD/Views/ItemList.swift
@@ -0,0 +1,18 @@
+//
+//  ItemList.swift
+//  FFWD
+//
+//  Created by Joseph DeWeese on 12/17/24.
+//
+
+import SwiftUI
+
+struct ItemList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemList()
+}
